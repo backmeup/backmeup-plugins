5e357b17555d9af7ca342b3541d7b76f09aabed3
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-05-04 22:02:12 +0200

    Implemented pdf generator

---
+++ backmeup-filegenerator-plugin/src/main/java/org/backmeup/filegenerator/generator/impl/PdfGenerator.java
@@ -0,0 +1,60 @@
+package org.backmeup.filegenerator.generator.impl;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+import org.backmeup.filegenerator.generator.Generator;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class PdfGenerator implements Generator {
+	private String title;
+	private String body;
+	
+	Font titleFont;
+	Font bodyFont;
+	
+	Document document;
+	
+	public PdfGenerator(String title, String body) {
+		titleFont = new Font(Font.FontFamily.HELVETICA, 22, Font.BOLD);
+		bodyFont = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.NORMAL);
+		
+		this.title = title;
+		this.body = body;
+		
+		this.document = new Document();
+	}
+	
+	@Override
+	public InputStream generate() {
+		try {
+			ByteArrayOutputStream osPdf = new ByteArrayOutputStream();
+			PdfWriter.getInstance(document, osPdf);
+
+			this.document.open();
+			document.addTitle(this.title);
+			document.addKeywords("Backmeup, backup");
+			document.addAuthor("backmeup");
+			document.addCreator("backmeup-filegenerator-plugin");
+
+			Paragraph content = new Paragraph();
+			content.add(new Paragraph(" "));
+			content.add(new Paragraph(this.title, titleFont));
+			content.add(new Paragraph(" "));
+			content.add(new Paragraph(this.body, bodyFont));
+
+			document.add(content);
+			document.close();
+			
+			return new ByteArrayInputStream(osPdf.toByteArray());
+		} catch (DocumentException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
