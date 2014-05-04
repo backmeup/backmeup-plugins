b021746b15d8ea8546b3ac9f4b45af855564cace
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-05-04 22:01:48 +0200

    Implemented plain text generator

---
+++ backmeup-filegenerator-plugin/src/main/java/org/backmeup/filegenerator/generator/impl/TextGenerator.java
@@ -0,0 +1,43 @@
+package org.backmeup.filegenerator.generator.impl;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
+import org.backmeup.filegenerator.generator.Generator;
+
+import de.svenjacobs.loremipsum.LoremIpsum;
+
+public class TextGenerator implements Generator {
+	private int amount;
+	private LoremIpsum loremIpsum;
+	
+	public TextGenerator(int amountInParagraphs) {
+		this.amount = amountInParagraphs;
+		this.loremIpsum = new LoremIpsum();
+	}
+	
+	@Override
+	public InputStream generate() {
+		String text = loremIpsum.getParagraphs(amount);
+		return stringToStream(text);
+	}
+	
+	public String getParagraphs(int amount) {
+		return loremIpsum.getParagraphs(amount);
+	}
+	
+	public String getWords(int amount) {
+		return loremIpsum.getWords(amount);
+	}
+	
+	private InputStream stringToStream(String input) {
+		try {
+			InputStream is = new ByteArrayInputStream(input.getBytes("UTF-8"));
+			return is;
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
