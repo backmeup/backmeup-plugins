a0f578e5cfe5157d580be79590535e1b1f29636e
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-09-01 15:24:41 +0200

    Fixed filesplitting action to comply with new plugin api Code formatting

--- backmeup-filesplitting-plugin/src/main/java/org/backmeup/plugin/api/actions/filesplitting/FilesplittDescribable.java
+++ backmeup-filesplitting-plugin/src/main/java/org/backmeup/plugin/api/actions/filesplitting/FilesplittDescribable.java
@@ -3,21 +3,18 @@ package org.backmeup.plugin.api.actions.filesplitting;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.backmeup.plugin.api.actions.BaseActionDescribable;
-//TODO move in own project
-public class FilesplittDescribable extends BaseActionDescribable { 
-  public FilesplittDescribable() {
-    super("filesplitt.properties");
-  }
-  
-  @Override
-  public List<String> getAvailableOptions ()
-  {
-	  List<String> options = new LinkedList<> ();
-	  
-	  options.add ("Test Option 1");
-	  options.add ("Test Option 2");
-	  
-	  return options;
-  }
+import org.backmeup.plugin.api.connectors.BaseActionDescribable;
+
+public class FilesplittDescribable extends BaseActionDescribable {
+	public FilesplittDescribable() {
+		super("filesplitt.properties");
+	}
+
+	@Override
+	public List<String> getAvailableOptions() {
+		List<String> options = new LinkedList<>();
+		options.add("Test Option 1");
+		options.add("Test Option 2");
+		return options;
+	}
 }
