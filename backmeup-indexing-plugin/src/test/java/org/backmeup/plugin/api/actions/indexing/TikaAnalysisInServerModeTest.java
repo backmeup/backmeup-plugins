1fd727756c8b5d175adade19756516b4d24b0244
Andrew Lindley <andrew.lindley@ait.ac.at>
2015-02-09 17:54:52 +0100

    removed FakeFileStorage mock implementation

--- backmeup-indexing-plugin/src/test/java/org/backmeup/plugin/api/actions/indexing/TikaAnalysisInServerModeTest.java
+++ backmeup-indexing-plugin/src/test/java/org/backmeup/plugin/api/actions/indexing/TikaAnalysisInServerModeTest.java
@@ -6,27 +6,22 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
-import java.util.Iterator;
 
 import org.apache.commons.io.IOUtils;
 import org.backmeup.plugin.api.MetainfoContainer;
 import org.backmeup.plugin.api.storage.DataObject;
-import org.backmeup.plugin.api.storage.Storage;
 import org.backmeup.plugin.api.storage.StorageException;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TikaAnalysisInServerModeTest {
 
     private TikaServerStub tikaServer;
-    DataObject pdf1 = getTestObject("creative-commons.pdf");
-    DataObject pdf2 = getTestObject("tika_analyser.pdf");
-    DataObject txt1 = getTestObject("tika_analyser.txt");
-    DataObject jpg1 = getTestObject("creative-commons.jpg");
-    DataObject png1 = getTestObject("creative-commons.png");
+    DataObject pdf1 = getTestDataObjects("creative-commons.pdf");
+    DataObject pdf2 = getTestDataObjects("tika_analyser.pdf");
+    DataObject txt1 = getTestDataObjects("tika_analyser.txt");
+    DataObject jpg1 = getTestDataObjects("creative-commons.jpg");
+    DataObject png1 = getTestDataObjects("creative-commons.png");
 
     @Before
     public void before() {
@@ -81,7 +76,13 @@ public class TikaAnalysisInServerModeTest {
         assertFalse(this.tikaServer.isTikaAlive());
     }
 
-    private DataObject getTestObject(final String filename) {
+    /**
+     * Small helper utility to get selected testdata in form ob a DataObject handle
+     * 
+     * @param filename
+     * @return
+     */
+    private DataObject getTestDataObjects(final String filename) {
         DataObject dob = new DataObject() {
             @Override
             public byte[] getBytes() throws IOException {
@@ -108,96 +109,6 @@ public class TikaAnalysisInServerModeTest {
             }
         };
         return dob;
-    }
-
-    public class FakeFileStorage implements Storage {
-
-        @Override
-        public Iterator<DataObject> getDataObjects() throws StorageException {
-            DataObject pdfDob = new DataObject() {
-
-                @Override
-                public byte[] getBytes() throws IOException {
-                    return IOUtils.toByteArray(new FileReader("src/test/resources/creative-commons.pdf"));
-                }
-
-                @Override
-                public long getLength() {
-                    return 21;
-                }
-
-                @Override
-                public String getPath() {
-                    return "/creative-commons.pdf";
-                }
-
-                @Override
-                public MetainfoContainer getMetainfo() {
-                    return null;
-                }
-
-                @Override
-                public void setMetainfo(MetainfoContainer meta) {
-                }
-            };
-            return Arrays.asList(pdfDob).iterator();
-        }
-
-        // here for tests...
-
-        @Override
-        public void open(String path) throws StorageException {
-            Assert.fail("do not call");
-        }
-
-        @Override
-        public void close() throws StorageException {
-            Assert.fail("do not call");
-        }
-
-        @Override
-        public void delete() throws StorageException {
-            Assert.fail("do not call");
-        }
-
-        @Override
-        public int getDataObjectCount() throws StorageException {
-            Assert.fail("do not call");
-            return 0;
-        }
-
-        @Override
-        public long getDataObjectSize() throws StorageException {
-            Assert.fail("do not call");
-            return 0;
-        }
-
-        @Override
-        public boolean existsPath(String path) throws StorageException {
-            Assert.fail("do not call");
-            return false;
-        }
-
-        @Override
-        public void addFile(InputStream is, String path, MetainfoContainer metadata) throws StorageException {
-            Assert.fail("do not call");
-        }
-
-        @Override
-        public void removeFile(String path) throws StorageException {
-            Assert.fail("do not call");
-        }
-
-        @Override
-        public void removeDir(String path) throws StorageException {
-            Assert.fail("do not call");
-        }
-
-        @Override
-        public void move(String fromPath, String toPath) throws StorageException {
-            Assert.fail("do not call");
-        }
-
     }
 
 }
