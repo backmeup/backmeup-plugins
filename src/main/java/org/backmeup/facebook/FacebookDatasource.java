b4279e344e223828f8f6b92f805864c258e7a23a
RStoeckl <sto16373@spengergasse.at>
2015-07-28 13:33:31 +0200

    fixed storage register

--- src/main/java/org/backmeup/facebook/FacebookDatasource.java
+++ src/main/java/org/backmeup/facebook/FacebookDatasource.java
@@ -31,7 +31,8 @@ public class FacebookDatasource implements Datasource
 	/**
 	 * 
 	 * @deprecated use standalone version instead
-	 * @param args arguments
+	 * @param args
+	 *            arguments
 	 */
 	@Deprecated
 	public static void main(String[] args)
@@ -129,11 +130,19 @@ public class FacebookDatasource implements Datasource
 		target = new File(tDir + "/html");
 		if (!target.exists())
 			target.mkdirs();
-		
+
 		FileUtils.exctractFromJar("/org/backmeup/facebook/htmlgenerator/css/main.css", new File("" + target + "/main.css"), HTMLGenerator.class);
 		FileUtils.exctractFromJar("/org/backmeup/facebook/htmlgenerator/css/menu.css", new File("" + target + "/menu.css"), HTMLGenerator.class);
 		mainGen.genOverview(target, dir);
 		// }
+		ArrayList<File> allXmlFiles = new ArrayList<>();
+		files(dir.getParentFile(), allXmlFiles);
+		ArrayList<File> allHtmlFiles = new ArrayList<>();
+		files(target, allHtmlFiles);
+		for (File file : allXmlFiles)
+			registerFile(dir.getParentFile(), file, storage);
+		for (File file : allHtmlFiles)
+			registerFile(target, file, storage);
 		try (FileInputStream fishtml = new FileInputStream(target); FileInputStream fisxml = new FileInputStream(dir.getParentFile()))
 		{
 			storage.addFile(fisxml, "xmldata", new MetainfoContainer());
@@ -144,4 +153,27 @@ public class FacebookDatasource implements Datasource
 		}
 	}
 
+	public static void files(File root, ArrayList<File> list)
+	{
+		if (root == null || list == null)
+			return;
+		if (root.isDirectory())
+			for (File file : root.listFiles())
+				if (file.isDirectory())
+					files(root, list);
+				else
+					list.add(file);
+	}
+
+	public static void registerFile(File root, File file, Storage storage)
+	{
+		try (FileInputStream fis = new FileInputStream(file))
+		{
+			String path = FileUtils.getWayTo(root.getParentFile(), file);
+			storage.addFile(fis, path.substring(2, path.length() - 1), new MetainfoContainer());
+		} catch (IOException | StorageException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
