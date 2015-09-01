328703b4dabce6721a3516e7657b91ddf2d01ecf
Wolfgang Eibner <we@x-net.at>
2015-09-01 14:46:45 +0200

    short version

--- backmeup-facebook-plugin/src/main/java/org/backmeup/facebook/utils/Mathematics.java
+++ backmeup-facebook-plugin/src/main/java/org/backmeup/facebook/utils/Mathematics.java
@@ -4,8 +4,6 @@ public class Mathematics
 {
 	public static double roundDouble(double number, int digitAfterKomma)
 	{
-		double komma = number - (int) number;
-		double cutaway = ((int) ((komma + 5 / Math.pow(10, digitAfterKomma + 1)) * Math.pow(10, digitAfterKomma))) / Math.pow(10, digitAfterKomma);
-		return ((int) number) + cutaway;
+		return Math.round(number * Math.pow(10, digitAfterKomma)) / Math.pow(10, digitAfterKomma);
 	}
 }
