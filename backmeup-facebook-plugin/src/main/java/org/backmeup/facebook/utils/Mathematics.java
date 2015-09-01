69050783d59c8ad29f96684adbbcde7df65657d5
Wolfgang Eibner <we@x-net.at>
2015-09-01 16:21:56 +0200

    delete unused Utility class

--- backmeup-facebook-plugin/src/main/java/org/backmeup/facebook/utils/Mathematics.java
+++
@@ -1,7 +0,0 @@
-package org.backmeup.facebook.utils;
-
-public class Mathematics {
-    public static double roundDouble(double number, int digitAfterKomma) {
-        return Math.round(number * Math.pow(10, digitAfterKomma)) / Math.pow(10, digitAfterKomma);
-    }
-}
