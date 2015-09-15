dbb04d2f4f32d3b38881fa88f9186b494c11a54d
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2015-09-15 14:26:05 +0200

    Renamed datasink to action Code formatting

--- backmeup-zip-plugin/src/main/java/org/backmeup/zip/ZipDescriptor.java
+++ backmeup-zip-plugin/src/main/java/org/backmeup/zip/ZipDescriptor.java
@@ -5,5 +5,5 @@ import org.backmeup.plugin.api.BaseActionDescribable;
 public class ZipDescriptor extends BaseActionDescribable {
     public ZipDescriptor() {
         super("zip.properties");
-   }
+    }
 }
