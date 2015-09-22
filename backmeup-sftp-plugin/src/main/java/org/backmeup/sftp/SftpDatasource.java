39c4e53c0bb19718be88d1879a70e1ef1b7317fd
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2015-09-22 13:21:38 +0200

    Replaced tabs with spaces Code formatting

--- backmeup-sftp-plugin/src/main/java/org/backmeup/sftp/SftpDatasource.java
+++ backmeup-sftp-plugin/src/main/java/org/backmeup/sftp/SftpDatasource.java
@@ -33,14 +33,13 @@ import com.jcraft.jsch.SftpATTRS;
 import com.jcraft.jsch.SftpException;
 
 /**
- * The DropboxDatasource is capable of listing all directories and files of a certain directory and of downloading
- * certain files from Dropbox.
+ * The SftpDatasource is capable of listing and downloading directories and
+ * files of a sftp server.
  * 
- * @author fschoeppl
  */
 public class SftpDatasource implements Datasource {
-	
-	private static final String SFTP = "sftp";
+
+    private static final String SFTP = "sftp";
 
     private final ResourceBundle textBundle = ResourceBundle.getBundle(SftpDatasource.class.getSimpleName());
     private static final String INDEX_HTML_WRAP = "org.backmeup.sftp.SftpDatasource.INDEX_HTML_WRAP";
@@ -58,50 +57,53 @@ public class SftpDatasource implements Datasource {
         info.setType(type);
         return info;
     }
-    
+
     @Override
-    public void downloadAll(PluginProfileDTO pluginProfile, PluginContext pluginContext,
-    						Storage storage, Progressable progressor) throws StorageException {
-    	Session session = null;
+    public void downloadAll(PluginProfileDTO pluginProfile, PluginContext pluginContext, Storage storage,
+            Progressable progressor) throws StorageException {
+        Session session = null;
         ChannelSftp sftpChannel = null;
-        
+
         try {
-        	JSch jsch = new JSch();
-        	
-        	Map<String, String> accessData = pluginProfile.getAuthData().getProperties();
+            JSch jsch = new JSch();
+
+            Map<String, String> accessData = pluginProfile.getAuthData().getProperties();
             progressor.progress("Connecting to sftp server " + accessData.get("Host"));
-            
-            session = jsch.getSession(accessData.get("Username"), accessData.get("Host"), Integer.parseInt(accessData.get("Port")));
+
+            session = jsch.getSession(accessData.get("Username"), accessData.get("Host"),
+                    Integer.parseInt(accessData.get("Port")));
             session.setConfig("StrictHostKeyChecking", "no");
             session.setPassword(accessData.get("Password"));
             session.connect(60000);
-           
+
             Channel channel = session.openChannel("sftp");
             channel.connect(60000);
             sftpChannel = (ChannelSftp) channel;
 
-            if(channel.isConnected()) {
-            	progressor.progress("Connection successfull");
+            if (channel.isConnected()) {
+                progressor.progress("Connection successfull");
             } else {
-                throw new PluginException(SftpDescriptor.SFTP_ID, "Could not connect to sftp server " + accessData.get("Host"));
+                throw new PluginException(SftpDescriptor.SFTP_ID,
+                        "Could not connect to sftp server " + accessData.get("Host"));
             }
 
             progressor.progress("Change to remote folder");
             sftpChannel.cd(accessData.get("Folder"));
-            
-            StringBuilder sb = new StringBuilder(); 
+
+            StringBuilder sb = new StringBuilder();
             handleDownloadAll(".", sftpChannel, storage, progressor, sb);
 
             String indexHtml = MessageFormat.format(this.textBundle.getString(INDEX_HTML_WRAP), sb.toString());
-        	storage.addFile(new ByteArrayInputStream(indexHtml.getBytes("UTF-8")), "index.html", new MetainfoContainer());
-        	
+            storage.addFile(new ByteArrayInputStream(indexHtml.getBytes("UTF-8")), "index.html",
+                    new MetainfoContainer());
+
             sftpChannel.exit();
             session.disconnect();
-            
+
             progressor.progress("Download completed");
 
         } catch (JSchException | SftpException | UnsupportedEncodingException e) {
-        	progressor.progress(e.toString());
+            progressor.progress(e.toString());
             progressor.progress(e.getMessage());
             throw new PluginException(SftpDescriptor.SFTP_ID, "An error occured during the backup", e);
         } finally {
@@ -113,38 +115,39 @@ public class SftpDatasource implements Datasource {
             }
         }
     }
-    
-    private void handleDownloadAll(String folder, ChannelSftp sftpChannel, Storage storage, Progressable progressor, StringBuilder sb) throws SftpException, StorageException {
-    	List<String> toVisit = new ArrayList<>();
-    	
+
+    private void handleDownloadAll(String folder, ChannelSftp sftpChannel, Storage storage, Progressable progressor,
+            StringBuilder sb) throws SftpException, StorageException {
+        List<String> toVisit = new ArrayList<>();
+
         progressor.progress("Get list of folders");
         @SuppressWarnings("unchecked")
         Vector<ChannelSftp.LsEntry> vEntries = sftpChannel.ls(folder);
         for (LsEntry entry : vEntries) {
-        	String fname = entry.getFilename();
-        	if (".".equals(fname) || "..".equals(fname)) {
-        		continue;
-        	}
-	        MetainfoContainer cont = new MetainfoContainer();
-	        SftpATTRS attrs = entry.getAttrs();
-	        if(attrs.isDir()) {
-	        	cont.addMetainfo(create("1", "directory", fname));
-	        	toVisit.add(entry.getFilename());
-	        } else {
-	        	String xName = folder+"/"+fname;
-	        	xName = xName.substring(2);
-	        	String type = new MimetypesFileTypeMap().getContentType(fname);
-		        cont.addMetainfo(create("1", type, fname));
-	        	InputStream is = sftpChannel.get(xName);
-	        	
-		        storage.addFile(is, xName, cont);
-	        	sb.append( MessageFormat.format(this.textBundle.getString(INDEX_HTML_ENTRY), xName, attrs.getPermissionsString(),
-	    				attrs.getSize(), attrs.getMtimeString(), xName ) );
-	        }
-	    }
-        
-        for (String f : toVisit) {
-        	handleDownloadAll(folder+"/"+f, sftpChannel, storage, progressor, sb);
+            String fname = entry.getFilename();
+            if (".".equals(fname) || "..".equals(fname)) {
+                continue;
+            }
+            MetainfoContainer cont = new MetainfoContainer();
+            SftpATTRS attrs = entry.getAttrs();
+            if (attrs.isDir()) {
+                cont.addMetainfo(create("1", "directory", fname));
+                toVisit.add(entry.getFilename());
+            } else {
+                String xName = folder + "/" + fname;
+                xName = xName.substring(2);
+                String type = new MimetypesFileTypeMap().getContentType(fname);
+                cont.addMetainfo(create("1", type, fname));
+                InputStream is = sftpChannel.get(xName);
+
+                storage.addFile(is, xName, cont);
+                sb.append(MessageFormat.format(this.textBundle.getString(INDEX_HTML_ENTRY), xName,
+                        attrs.getPermissionsString(), attrs.getSize(), attrs.getMtimeString(), xName));
+            }
         }
-    } 
+
+        for (String f : toVisit) {
+            handleDownloadAll(folder + "/" + f, sftpChannel, storage, progressor, sb);
+        }
+    }
 }
--- backmeup-sftp-plugin/src/main/java/org/backmeup/sftp/SftpDescriptor.java
+++ backmeup-sftp-plugin/src/main/java/org/backmeup/sftp/SftpDescriptor.java
@@ -7,47 +7,47 @@ import org.backmeup.plugin.api.BaseSourceSinkDescribable;
 import org.backmeup.plugin.api.Metadata;
 
 /**
- * The DropboxDescriptor provides all necessary information about this plugin.
- * Note: DROPBOX_ID matches the filters stated in the configuration files:
- * META-INF/spring/org.backmeup.dropbox-context.xml
- * META-INF/spring/org.backmeup.dropbox-osgi-context.xml
+ * The SftpDescriptor provides all necessary information about this plugin.
+ * Note: SFTP_ID matches the filters stated in the configuration files:
+ * META-INF/spring/org.backmeup.sftp-context.xml
+ * META-INF/spring/org.backmeup.sftp-osgi-context.xml
  * 
  * @author fschoeppl
  */
 public class SftpDescriptor extends BaseSourceSinkDescribable {
-	public static final String SFTP_ID = "org.backmeup.sftp";
+    public static final String SFTP_ID = "org.backmeup.sftp";
 
-	@Override
-	public String getId() {
-		return SFTP_ID;
-	}
+    @Override
+    public String getId() {
+        return SFTP_ID;
+    }
 
-	@Override
-	public String getTitle() {
-		return "BackMeUp sftp Plug-In";
-	}
+    @Override
+    public String getTitle() {
+        return "BackMeUp sftp Plug-In";
+    }
 
-	@Override
-	public String getDescription() {
-		return "A plug-in that is capable of downloading data from sftp";
-	}
+    @Override
+    public String getDescription() {
+        return "A plug-in that is capable of downloading data from sftp";
+    }
 
-	@Override
-	public String getImageURL() {
-		return "http://about:blank";
-	}
+    @Override
+    public String getImageURL() {
+        return "http://about:blank";
+    }
 
-	@Override
-	public PluginType getType() {
-		return PluginType.Source;
-	}
+    @Override
+    public PluginType getType() {
+        return PluginType.Source;
+    }
 
-	@Override
-	public Map<String, String> getMetadata(Map<String, String> accessData) {
-		Map<String, String> metadata = new HashMap<String, String>();
-		metadata.put(Metadata.BACKUP_FREQUENCY, "daily");
-		metadata.put(Metadata.DYNAMIC_OPTIONS, "true");
-		return metadata;
-	}
+    @Override
+    public Map<String, String> getMetadata(Map<String, String> accessData) {
+        Map<String, String> metadata = new HashMap<String, String>();
+        metadata.put(Metadata.BACKUP_FREQUENCY, "daily");
+        metadata.put(Metadata.DYNAMIC_OPTIONS, "true");
+        return metadata;
+    }
 
 }
