44f190efb11037f20483c43d952d75e4ac69126c
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2015-09-22 11:00:16 +0200

    Replaced tabs with spaces

--- backmeup-thumbnail-plugin/src/main/java/org/backmeup/plugin/api/actions/thumbnail/ThumbnailDescribable.java
+++ backmeup-thumbnail-plugin/src/main/java/org/backmeup/plugin/api/actions/thumbnail/ThumbnailDescribable.java
@@ -3,7 +3,7 @@ package org.backmeup.plugin.api.actions.thumbnail;
 import org.backmeup.plugin.api.BaseActionDescribable;
 
 public class ThumbnailDescribable extends BaseActionDescribable {
-	public ThumbnailDescribable() {
-		 super("thumbnail.properties");
-	}
+    public ThumbnailDescribable() {
+        super("thumbnail.properties");
+    }
 }
