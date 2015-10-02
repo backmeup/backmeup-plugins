956e812f27b52339d8d9019cd9b0cbd730ba54ab
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2015-10-02 21:11:58 +0200

    Declare modifiers in correct order

--- backmeup-mail-plugin/src/main/java/org/backmeup/mail/core/MediaType.java
+++ backmeup-mail-plugin/src/main/java/org/backmeup/mail/core/MediaType.java
@@ -1,16 +1,16 @@
 package org.backmeup.mail.core;
 
 public final class MediaType {
+    public static final String TEXT = "text/*";
+    public static final String TEXT_PLAIN = "text/plain";
+    public static final String TEXT_HTML = "text/html";
+    
+    public static final String MULTIPART = "multipart/*";
+    public static final String MULTIPART_ALTERNATIVE = "multipart/alternative";
+    
+    public static final String MESSAGE_RFC822 = "message/rfc822";
+    
     private MediaType() {
         // Utility classes should not have public constructor
     }
-    
-    public final static String TEXT = "text/*";
-    public final static String TEXT_PLAIN = "text/plain";
-    public final static String TEXT_HTML = "text/html";
-    
-    public final static String MULTIPART = "multipart/*";
-    public final static String MULTIPART_ALTERNATIVE = "multipart/alternative";
-    
-    public final static String MESSAGE_RFC822 = "message/rfc822";
 }
